a39592cf56b747a99a4b89c68339a16b5993b37a
José Simões <jose.simoes@eclo.solutions>
2019-12-05 13:32:19 +0000

    Add unit tests
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ MetadataProcessor.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("nanoFramework.MetadataProcessor.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("nanoFramework project contributors")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright (c) 2019 The nanoFramework project contributors")]
