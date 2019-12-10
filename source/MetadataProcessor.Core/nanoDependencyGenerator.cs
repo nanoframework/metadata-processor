8ff3f54582f3d37f33d3355870289d900ee18211
José Simões <jose.simoes@eclo.solutions>
2019-12-10 16:57:13 +0000

    Add support for dependency generator
    - WIP still missing the ComputeAssemblyHash, ComputeTypeHash and list referenced assembly types.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ source/MetadataProcessor.Core/nanoDependencyGenerator.cs
@@ -0,0 +1,67 @@
+﻿//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using Mono.Cecil;
+using nanoFramework.Tools.MetadataProcessor.Core.Extensions;
+using Stubble.Core.Builders;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace nanoFramework.Tools.MetadataProcessor.Core
+{
+    /// <summary>
+    /// Generates dependency graph for a .NET nanoFramework assembly.
+    /// </summary>
+    public sealed class nanoDependencyGenerator
+    {
+        private readonly nanoTablesContext _tablesContext;
+        private readonly AssemblyDefinition _assemblyDefinition;
+        private readonly string _path;
+        private readonly string _name;
+        private readonly string _project;
+
+        private string _assemblyName;
+        private nanoTablesContext tablesContext;
+        private string fileName;
+        private string _fileName;
+
+        public nanoDependencyGenerator(
+            AssemblyDefinition assemblyDefinition, 
+            nanoTablesContext tablesContext, 
+            string fileName)
+        {
+            _assemblyDefinition = assemblyDefinition;
+            _tablesContext = tablesContext;
+            _fileName = fileName;
+        }
+
+        public nanoDependencyGenerator(
+            nanoTablesContext tablesContext,
+            string path,
+            string name,
+            string project,
+            bool interopCode)
+        {
+            _tablesContext = tablesContext;
+            _path = path;
+            _name = name;
+            _project = project;
+
+        }
+
+        public void Write(
+            XmlWriter xmlWriter)
+        {
+            var dependencyWriter = new nanoDependencyGeneratorWriter(
+                _assemblyDefinition,
+                _tablesContext);
+            dependencyWriter.Write(xmlWriter);
+        }
+
+    }
+}
