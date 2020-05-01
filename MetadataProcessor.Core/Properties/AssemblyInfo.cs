ef7d528dcf5cb45b424109ebfcfd5f18d47079f0
SandorDobos <dbs.sndr@gmail.com>
2020-05-01 22:57:15 +0200

    Sign test assembly and make Core's internals visible to it.

--- MetadataProcessor.Core/Properties/AssemblyInfo.cs
+++ MetadataProcessor.Core/Properties/AssemblyInfo.cs
@@ -8,4 +8,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("nanoFramework project contributors")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Copyright (c) .NET Foundation and Contributors")]
+[assembly: AssemblyCopyright ("Copyright (c) 2019 The nanoFramework project contributors")]
+
+[assembly: InternalsVisibleTo("nanoFramework.Tools.MetadataProcessor.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010035b49090a010c2970b2f8df8f66ad47a09583d654c0f2375980cf4afcfb48141ec9fd27d464b93a3216545588a5119d779c0ec66eed7c715567c2315cb908f70403b3bb59018b34096ad29c1c91f8d3c83ad407052ead9112e694660dec0ce7a361bff67781cf041e19d78afd128603dd188eb238186ac5aa6caf418891d5bb2")]
