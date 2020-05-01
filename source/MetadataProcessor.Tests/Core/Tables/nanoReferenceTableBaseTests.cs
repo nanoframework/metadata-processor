a348c8324fbfb5a6132fd1a7ac8ab24bd2a448c4
SandorDobos <dbs.sndr@gmail.com>
2020-05-01 23:22:05 +0200

    nanoReferenceTableBaseTests added

---
+++ source/MetadataProcessor.Tests/Core/Tables/nanoReferenceTableBaseTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mono.Cecil;
+using System.IO;
+using System.Text;
+
+namespace nanoFramework.Tools.MetadataProcessor.Tests.Core.Tables
+{
+    [TestClass]
+    public class nanoReferenceTableBaseTests
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var items = new List<object>() { 1, 2, 3 };
+
+            var comparer = new ObjectComparer();
+            var context = TestObjectHelper.GetInitializedNanoTablesContext();
+
+            // test
+            var iut = new TestNanoReferenceTable(items, comparer, context);
+
+            Assert.IsTrue(comparer.EqualsCallParameters.Any() || comparer.GetHashCodeCallParameters.Any());
+        }
+
+        [TestMethod]
+        public void ForEachItemsTest()
+        {
+            var items = new List<object>() { 1, 2, 3 };
+
+            var comparer = new ObjectComparer();
+            var context = TestObjectHelper.GetInitializedNanoTablesContext();
+            var iut = new TestNanoReferenceTable(items, comparer, context);
+
+            // test
+            var forEachCalledOnItems = new List<object>();
+            iut.ForEachItems((idx, item) =>
+            {
+                forEachCalledOnItems.Add(item);
+                Assert.AreEqual(items.IndexOf(item), (int)idx);
+            });
+
+            CollectionAssert.AreEqual(items.ToArray(), forEachCalledOnItems.ToArray());
+        }
+
+        [TestMethod]
+        public void WriteTest()
+        {
+            var items = new List<object>() { 3, 2, 1 };
+
+            var comparer = new ObjectComparer();
+            var context = TestObjectHelper.GetInitializedNanoTablesContext();
+            var iut = new TestNanoReferenceTable(items, comparer, context);
+            using (var ms = new MemoryStream())
+            {
+                using (var bw = new System.IO.BinaryWriter(ms, Encoding.Default, true))
+                {
+                    var writer = nanoBinaryWriter.CreateLittleEndianBinaryWriter(bw);
+
+                    // test
+                    iut.Write(writer);
+
+                    bw.Flush();
+
+                }
+
+                var bytesWritten = ms.ToArray();
+                CollectionAssert.AreEqual(new byte[] { 0x33, 0, 0x32, 0, 0x31, 0 }, bytesWritten, String.Join(", ", bytesWritten.Select(i => i.ToString("X"))));
+            }
+        }
+
+        private class TestNanoReferenceTable : nanoReferenceTableBase<object>
+        {
+            public List<HashSet<MetadataToken>> RemoveUnusedItemsCallParameters = new List<HashSet<MetadataToken>>();
+            public List<object> AllocateSingleItemStringsCallParameters = new List<object>();
+            public List<object> WriteSingleItemCallParameters = new List<object>();
+
+            public TestNanoReferenceTable(IEnumerable<object> nanoTableItems, IEqualityComparer<object> comparer, nanoTablesContext context) : base(nanoTableItems, comparer, context)
+            {
+            }
+
+            protected override void WriteSingleItem(nanoBinaryWriter writer, object item)
+            {
+                this.WriteSingleItemCallParameters.Add(item);
+                writer.WriteString(item.ToString());
+            }
+
+            public override void RemoveUnusedItems(HashSet<MetadataToken> set)
+            {
+                this.RemoveUnusedItemsCallParameters.Add(set);
+                base.RemoveUnusedItems(set);
+            }
+
+            protected override void AllocateSingleItemStrings(object item)
+            {
+                this.AllocateSingleItemStringsCallParameters.Add(item);
+                base.AllocateSingleItemStrings(item);
+            }
+        }
+
+        private class ObjectComparer : IEqualityComparer<object>
+        {
+            public List<KeyValuePair<object, object>> EqualsCallParameters = new List<KeyValuePair<object, object>>();
+            public List<object> GetHashCodeCallParameters = new List<object>();
+
+            public new bool Equals(object x, object y)
+            {
+                this.EqualsCallParameters.Add(new KeyValuePair<object, object>(x, y));
+                return Object.Equals(x, y);
+            }
+
+            public int GetHashCode(object obj)
+            {
+                this.GetHashCodeCallParameters.Add(obj);
+                return obj.GetHashCode();
+            }
+        }
+
+    }
+}
---
+++ source/MetadataProcessor.Tests/TestObjectHelper.cs
@@ -0,0 +1,38 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nanoFramework.Tools.MetadataProcessor.Tests
+{
+    public static class TestObjectHelper
+    {
+        public static nanoTablesContext GetInitializedNanoTablesContext()
+        {
+            nanoTablesContext ret = null;
+
+            var assemblyDefinition = GetTestAssemblyDefinition();
+
+            ret = new nanoTablesContext(
+                assemblyDefinition, 
+                null,
+                new List<string>(),
+                null,
+                false,
+                false,
+                false);
+
+            return ret;
+        }
+
+        public static AssemblyDefinition GetTestAssemblyDefinition()
+        {
+            AssemblyDefinition ret = AssemblyDefinition.ReadAssembly(Assembly.GetExecutingAssembly().Location);
+            return ret;
+        }
+    }
+}
