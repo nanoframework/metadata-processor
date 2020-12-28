99bbb117c80c7863447f759d83ea2436ea2f0b8a
josesimoes <jose.simoes@eclo.solutions>
2020-12-28 10:42:56 +0000

    Add MSBuild task with MDP
    - Update azure build pipeline to pack new NuGet.
    - Update PS1 to update VS extension dependents.

---
+++ MetadataProcessor.MsBuildTask/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(".NET nanoFramework Metadata processor MSBuild Task library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("nanoFramework")]
+[assembly: AssemblyProduct(".NET nanoFramework Metadata processor MSBuild Task library")]
+[assembly: AssemblyCopyright("Copyright ©  2020 nanoFramework contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
