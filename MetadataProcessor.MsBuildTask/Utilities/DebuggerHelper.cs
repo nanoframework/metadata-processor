493e089217b0163dd5f2f1e706e3386e766b3afa
josesimoes <jose.simoes@eclo.solutions>
2021-01-06 19:02:47 +0000

    Improvements in output
    - Verbose now writes to log file instead of console.
    - On waiting for debugger to connect now outputs message to build output including process ID and name.

--- MetadataProcessor.MsBuildTask/Utilities/DebuggerHelper.cs
+++ MetadataProcessor.MsBuildTask/Utilities/DebuggerHelper.cs
@@ -4,6 +4,8 @@
 // See LICENSE file in the project root for full license information.
 //
 
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 using System;
 using System.Diagnostics;
 using System.Threading;
@@ -12,7 +14,7 @@ namespace nanoFramework.Tools.Utilities
 {
     internal static class DebuggerHelper
     {
-        public static void WaitForDebuggerIfEnabled(string varName, int timeoutSeconds = 30)
+        public static void WaitForDebuggerIfEnabled(string varName, TaskLoggingHelper logger, int timeoutSeconds = 30)
         {
 
             // this wait should be only available on debug build
@@ -24,13 +26,10 @@ namespace nanoFramework.Tools.Utilities
 
             if (!string.IsNullOrEmpty(debugEnabled) && debugEnabled.Equals("1", StringComparison.Ordinal))
             {
-                Console.WriteLine($".NET nanoFramework Metadata Processor msbuild instrumentation task debugging is enabled. Waiting {timeoutSeconds} seconds for debugger attachment...");
-
                 var currentProcessId = Process.GetCurrentProcess().Id;
                 var currentProcessName = Process.GetProcessById(currentProcessId).ProcessName;
-                Console.WriteLine(
-                    string.Format("Process Id: {0}, Name: {1}", currentProcessId, currentProcessName)
-                    );
+
+                logger.LogMessage(MessageImportance.Normal, $"Debugging of .NET nanoFramework Metadata Processor msbuild task is enabled. Waiting {timeoutSeconds} seconds for debugger attachment on Process Id: {currentProcessId} Name: {currentProcessName}...");
 
                 // wait N seconds for debugger to attach
                 while (!Debugger.IsAttached && waitForDebugToAttach.TotalSeconds > 0)
