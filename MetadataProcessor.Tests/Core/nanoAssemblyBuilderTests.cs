132fb12929c249c418c9ef58c61eca5a6f00555d
josesimoes <jose.simoes@eclo.solutions>
2021-01-07 11:08:37 +0000

    Fix wrong table count
    - Add property to expose this.
    - Update AssemblyBuild code accordingly.
    - Add unit test.
    - Rework strong key signing of MDP.Core and Tests to properly expose internals.

---
+++ MetadataProcessor.Tests/Core/nanoAssemblyBuilderTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace nanoFramework.Tools.MetadataProcessor.Tests.Core
+{
+    [TestClass]
+    public class nanoAssemblyBuilderTests
+    {
+        [TestMethod]
+        public void AssemblyTablesCountTest()
+        {
+            var nanoTablesContext = TestObjectHelper.GetTestNFAppNanoTablesContext();
+
+            Assert.IsTrue(
+                nanoAssemblyBuilder.GetTables(nanoTablesContext).Count() == nanoAssemblyBuilder.TablesCount,
+                "Tables count from context doesn't match the nanoAssemblyBuilder.TablesCount property.");
+        }
+    }
+}
