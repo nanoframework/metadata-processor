85b9faa33115ed577c1bfc04ea25c2fdd301ad8d
josesimoes <jose.simoes@eclo.solutions>
2021-01-12 23:33:23 +0000

    Improve TestApp with more cases and variations of generic params and args

--- MetadataProcessor.Tests/TestNFApp/GenericClass.cs
+++ MetadataProcessor.Tests/TestNFApp/GenericClass.cs
@@ -2,168 +2,175 @@
 
 namespace TestNFApp
 {
+    interface IDo
+    {
+        void Do1();
+        void Do2();
+    }
+
+    interface IDoThat
+    {
+        void DoThat1();
+        void DoThat2();
+    }
+
+    class ClassDoThis : IDo
+    {
+        public void Do1()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThis.Do1)}");
+        }
+
+        public void Do2()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThis.Do2)}");
+        }
+
+        public void DoThis1()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThis.DoThis1)}");
+        }
+
+        public void DoThis2()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThis.DoThis2)}");
+        }
+    }
+
+    class ClassDoInt : IDo
+    {
+        public void Do1()
+        {
+            Debug.WriteLine($"{nameof(ClassDoInt.Do1)}");
+        }
+
+        public void Do2()
+        {
+            Debug.WriteLine($"{nameof(ClassDoInt.Do2)}");
+        }
+    }
+
+    class ClassDoThatInt : IDo, IDoThat
+    {
+        public void Do1()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThatInt.Do1)}");
+        }
+
+        public void Do2()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThatInt.Do2)}");
+        }
+
+        public void DoThat1()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThatInt.DoThat1)}");
+        }
+
+        public void DoThat2()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThatInt.DoThat2)}");
+        }
+
+    }
+
+    class ClassDoThisAndThat : ClassDoThis, IDoThat
+    {
+        public void DoThat1()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThisAndThat.DoThat1)}");
+        }
+
+        public void DoThat2()
+        {
+            Debug.WriteLine($"{nameof(ClassDoThisAndThat.DoThat2)}");
+        }
+    }
+
+    public class ClassDoString : IDo
+    {
+        public void Do1()
+        {
+            Debug.WriteLine($"{nameof(ClassDoString.Do1)}");
+        }
+
+        public void Do2()
+        {
+            Debug.WriteLine($"{nameof(ClassDoString.Do2)}");
+        }
+    }
+
+    class GenericClass<T>
+    {
+        public void InstanceGenericDoOne(T t)
+        {
+            T v = t;
+
+            Debug.WriteLine($"{nameof(InstanceGenericDoOne)}-> {v}");
+        }
+
+        public void InstanceGenericDoTwo<T2>(T p1, T2 p2)
+        {
+            T v1 = p1;
+            T2 v2 = p2;
+
+            Debug.WriteLine($"{nameof(InstanceGenericDoTwo)}-> {v1},{v2}");
+        }
+
+        public void InstanceGenericDoOneOther<T1>(T1 t)
+        {
+            T1 v1 = t;
+
+            Debug.WriteLine($"{nameof(InstanceGenericDoOneOther)}-> {v1}");
+        }
+    }
+
     public class GenericClassTests
     {
+        private static void StaticGenericDo<T1, T2>(T1 val, T2 val2) where T1 : IDo where T2 : IDo
+        {
+            Debug.WriteLine($">>{nameof(StaticGenericDo)}");
+
+            val.Do1();
+            val.Do2();
+            val2.Do1();
+            val2.Do2();
+        }
+
+        private static void StaticGenericDoThisAndThat<T1, T2>(T1 val, T2 val2) where T1 : ClassDoThis, IDo where T2 : ClassDoThatInt
+        {
+            Debug.WriteLine($">>{nameof(StaticGenericDoThisAndThat)}");
+
+            val.DoThis1();
+            val.DoThis2();
+            val2.DoThat1();
+            val2.DoThat2();
+        }
+
         public GenericClassTests()
         {
             Debug.WriteLine("++++++++++++++++++++");
             Debug.WriteLine("++ Generics Tests ++");
             Debug.WriteLine("++++++++++++++++++++");
             Debug.WriteLine("");
-            
+
             var m = new GenericClass<int>();
             m.InstanceGenericDoOne(1);
-            m.InstanceGenericDoTwo(1, "OK");
+            m.InstanceGenericDoTwo(1, "ONE");
+            m.InstanceGenericDoOneOther(true);
 
             var m2 = new GenericClass<string>();
-            m2.InstanceGenericDoOne("OK");
-            m2.InstanceGenericDoTwo("OK", "Now");
+            m2.InstanceGenericDoOne("ONE");
+            m2.InstanceGenericDoTwo("ONE", "TWO");
+            m2.InstanceGenericDoOneOther(true);
 
             StaticGenericDo(new ClassDoInt(), new ClassDoString());
 
             StaticGenericDo(new ClassDoString(), new ClassDoInt());
 
             StaticGenericDoThisAndThat(new ClassDoThis(), new ClassDoThatInt());
-        }
-        private interface IDo
-        {
-            void Do1();
-            void Do2();
-        }
 
-        private interface IDoThat
-        {
-            void DoThat1();
-            void DoThat2();
-        }
-
-        private class ClassDoThis : IDo
-        {
-            public void Do1()
-            {
-                Debug.WriteLine($"{nameof(ClassDoThis.Do1)}");
-            }
-
-            public void Do2()
-            {
-                Debug.WriteLine($"{nameof(ClassDoThis.Do2)}");
-            }
-
-            public void DoThis1()
-            {
-                Debug.WriteLine($"{nameof(ClassDoThis.DoThis1)}");
-            }
-
-            public void DoThis2()
-            {
-                Debug.WriteLine($"{nameof(ClassDoThis.DoThis2)}");
-            }
-        }
-
-        private static void StaticGenericDo<T1, T2>(T1 val, T2 val2) where T1 : IDo where T2 : IDo
-        {
-            Debug.WriteLine($">> {nameof(StaticGenericDo)}");
-            
-            val.Do1();
-            val.Do2();
-            val2.Do1();
-            val2.Do2();
-        }
-        private static void StaticGenericDoThisAndThat<T1, T2>(T1 val, T2 val2) where T1 : ClassDoThis, IDo where T2 : ClassDoThatInt
-        {
-            Debug.WriteLine($">> {nameof(StaticGenericDoThisAndThat)}");
-
-            val.DoThis1();
-            val.DoThis1();
-            val2.DoThat1();
-            val2.DoThat2();
-        }
-
-        private class ClassDoInt : IDo
-        {
-            public void Do1()
-            {
-                Debug.WriteLine($"{nameof(ClassDoInt.Do1)}");
-            }
-
-            public void Do2()
-            {
-                Debug.WriteLine($"{nameof(ClassDoInt.Do2)}");
-            }
-        }
-
-        private class ClassDoThatInt : IDo, IDoThat
-        {
-            public void Do1()
-            {
-                Debug.WriteLine($"{nameof(ClassDoThatInt.Do1)}");
-            }
-
-            public void Do2()
-            {
-                Debug.WriteLine($"{nameof(ClassDoThatInt.Do2)}");
-            }
-
-            public void DoThat1()
-            {
-                Debug.WriteLine($"{nameof(ClassDoThatInt.DoThat1)}");
-            }
-
-            public void DoThat2()
-            {
-                Debug.WriteLine($"{nameof(ClassDoThatInt.DoThat2)}");
-            }
-
-        }
-
-        private class ClassDoThisAndInt : ClassDoThis, IDo
-        {
-            public void Do1()
-            {
-                Debug.WriteLine($"{nameof(ClassDoInt.Do1)}");
-            }
-
-            public void Do2()
-            {
-                Debug.WriteLine($"{nameof(ClassDoInt.Do2)}");
-            }
-        }
-
-        public class ClassDoString : IDo
-        {
-            public void Do1()
-            {
-                Debug.WriteLine($"{nameof(ClassDoString.Do1)}");
-            }
-
-            public void Do2()
-            {
-                Debug.WriteLine($"{nameof(ClassDoString.Do2)}");
-            }
-        }
-
-        class GenericClass<T>
-        {
-            public void InstanceGenericDoOne(T t)
-            {
-                T t2 = t;
-
-                Debug.WriteLine($"{nameof(InstanceGenericDoOne)}-{t2}");
-            }
-
-            public void InstanceGenericDoTwo<T2>(T t, T2 t2)
-            {
-                T _t = t;
-
-                Debug.WriteLine($"{nameof(InstanceGenericDoTwo)}- {_t}-{t2}");
-            }
-
-            public void InstanceGenericDoOneThat<T>(T t)
-            {
-                T t1 = t;
-
-                Debug.WriteLine($"{nameof(InstanceGenericDoOneThat)}-{t1}");
-            }
+            StaticGenericDoThisAndThat(new ClassDoThisAndThat(), new ClassDoThatInt());
         }
     }
 }
