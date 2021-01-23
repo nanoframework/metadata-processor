9c11510a06ac9aea2abb2ed3fa1e2194d581bbc2
josesimoes <jose.simoes@eclo.solutions>
2021-01-23 09:10:12 +0000

    GenericParameters now contain parameter type

--- MetadataProcessor.Shared/Tables/nanoGenericParamTable.cs
+++ MetadataProcessor.Shared/Tables/nanoGenericParamTable.cs
@@ -7,6 +7,7 @@ using Mono.Cecil;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace nanoFramework.Tools.MetadataProcessor
 {
@@ -49,6 +50,12 @@ namespace nanoFramework.Tools.MetadataProcessor
         }
 
         /// <summary>
+        /// Maps each unique generic parameter and its type.
+        /// </summary>
+        private Dictionary<GenericParameter, TypeReference> _typeForGenericParam =
+            new Dictionary<GenericParameter, TypeReference>();
+
+        /// <summary>
         /// Creates new instance of <see cref="nanoGenericParamTable"/> object.
         /// </summary>
         /// <param name="items">List of member references in Mono.Cecil format.</param>
@@ -60,6 +67,38 @@ namespace nanoFramework.Tools.MetadataProcessor
             nanoTablesContext context)
             : base(items, new GenericParameterComparer(), context)
         {
+            foreach(var gp in items)
+            {
+                var methodWithGenericParam = _context.MethodDefinitionTable.Items.SingleOrDefault(m => m.GenericParameters.Contains(gp));
+
+                if(methodWithGenericParam != null)
+                {
+                    // get the first method specification that matches this type AND name
+                    var instanceMethod = _context.MethodSpecificationTable.Items.SingleOrDefault(
+                        mr => mr.DeclaringType.GetElementType() == methodWithGenericParam.DeclaringType &&
+                        mr.Name == methodWithGenericParam.Name) as GenericInstanceMethod;
+
+                    _typeForGenericParam.Add(gp, instanceMethod.GenericArguments.ElementAt(gp.Position));
+                }
+                else
+                {
+                    var typeWithGenericParam = _context.TypeDefinitionTable.Items.SingleOrDefault(t => t.GenericParameters.Contains(gp));
+
+                    if(typeWithGenericParam != null)
+                    {
+                        // get the first member that matches this type
+                        var genericInstance = _context.MemberReferencesTable.Items.First(
+                            mr => mr.DeclaringType.GetElementType() == typeWithGenericParam)
+                            .DeclaringType as GenericInstanceType;
+
+                        _typeForGenericParam.Add(gp, genericInstance.GenericArguments.ElementAt(gp.Position));
+                    }
+                    else
+                    {
+                        Debug.Fail("Can't find generic parameter in either methods or type definitions");
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -93,7 +132,7 @@ namespace nanoFramework.Tools.MetadataProcessor
             // flags
             writer.WriteUInt16((ushort)item.Attributes);
 
-            // owner
+            // find owner
             ushort tag;
 
             if (item.Owner is TypeDefinition &&
@@ -118,7 +157,11 @@ namespace nanoFramework.Tools.MetadataProcessor
             // OR with tag to form coded index
             owner |= tag;
 
+            // owner
             writer.WriteUInt16(owner);
+
+            // Signature
+            writer.WriteUInt16(_context.SignaturesTable.GetOrCreateSignatureId(_typeForGenericParam[item]));
 
             // name
             WriteStringReference(writer, item.Name);
