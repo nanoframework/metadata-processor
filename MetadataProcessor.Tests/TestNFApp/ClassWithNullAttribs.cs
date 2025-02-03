79db55648a1197346f9e4ca8e01015b2e95ca227
José Simões <jose.simoes@eclo.solutions>
2025-02-03 18:25:34 +0000

    Improve processing of custom attributes (#173)

---
+++ MetadataProcessor.Tests/TestNFApp/ClassWithNullAttribs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TestNFApp
+{
+    public class ClassWithNullAttribs
+    {
+        [DoesNotReturn]
+        public int MethodDecoratedWithDoesNotReturn()
+        {
+            throw new Exception();
+        }
+
+        public string MethoWIthParamNotNullAttrib([NotNull] string value)
+        {
+            return value;
+        }
+    }
+}
