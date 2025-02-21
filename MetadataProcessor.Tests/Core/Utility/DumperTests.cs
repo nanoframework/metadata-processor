0cc2ae94d3eeff7841dc2a96d3533b3d66656d51
José Simões <jose.simoes@eclo.solutions>
2025-02-21 13:42:36 +0000

    Improve assembly dump
    - Output of locals, calls, strings, files and types now includes full names.
    - Update and improve unit tests as needed.
    - Replace Debug.Writeline with Console for NF test app.
    - Minor code style and fixes.

--- MetadataProcessor.Tests/Core/Utility/DumperTests.cs
+++ MetadataProcessor.Tests/Core/Utility/DumperTests.cs
@@ -1,12 +1,11 @@
-﻿//
-// Copyright (c) .NET Foundation and Contributors
-// See LICENSE file in the project root for full license information.
-//
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using nanoFramework.Tools.MetadataProcessor.Core;
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nanoFramework.Tools.MetadataProcessor.Core;
 
 namespace nanoFramework.Tools.MetadataProcessor.Tests.Core.Utility
 {
@@ -16,12 +15,12 @@ namespace nanoFramework.Tools.MetadataProcessor.Tests.Core.Utility
         [TestMethod]
         public void DumpAssemblyTest()
         {
-            var nanoTablesContext = TestObjectHelper.GetTestNFAppNanoTablesContext();
+            nanoTablesContext nanoTablesContext = TestObjectHelper.GetTestNFAppNanoTablesContext();
 
-            var fileNameBase = $"{DateTime.UtcNow.ToShortDateString()}_{DateTime.UtcNow.ToLongTimeString()}";
+            string fileNameBase = $"{DateTime.UtcNow.ToShortDateString()}_{DateTime.UtcNow.ToLongTimeString()}";
             fileNameBase = fileNameBase.Replace(':', '_').Replace('/', '_');
 
-            var dumpFileName = Path.Combine(TestObjectHelper.TestExecutionLocation, $"{fileNameBase}_dump.txt");
+            string dumpFileName = Path.Combine(TestObjectHelper.TestExecutionLocation, $"{fileNameBase}_dump.txt");
 
             nanoDumperGenerator dumper = new nanoDumperGenerator(
                 nanoTablesContext,
@@ -29,40 +28,47 @@ namespace nanoFramework.Tools.MetadataProcessor.Tests.Core.Utility
             dumper.DumpAll();
 
             // read back file
-            var dumpFileContent = File.ReadAllText(dumpFileName);
+            string dumpFileContent = File.ReadAllText(dumpFileName);
 
-            // search for bits
+            // output for debugging, if needed
+            Console.WriteLine($">>>>>>>\r\n{dumpFileContent}\r\n>>>>>>>");
+
+            // search for several bits
 
             // AssemblyRefs
-            Assert.IsTrue(dumpFileContent.Contains("AssemblyRefProps [23000001]: Flags: 00000000 'mscorlib'"));
-            Assert.IsTrue(dumpFileContent.Contains("AssemblyRefProps [23000002]: Flags: 00000000 'TestNFClassLibrary'"));
+            Assert.IsTrue(dumpFileContent.Contains("AssemblyRefProps [23000001]: Flags: 00000000 'mscorlib'"), "Wrong entry for mscorlib in assembly ref");
+            Assert.IsTrue(dumpFileContent.Contains("AssemblyRefProps [23000002]: Flags: 00000000 'TestNFClassLibrary'"), "Wrong entry for TestNFClassLibrary in assembly ref");
 
             // TypeRefs
-            Assert.IsTrue(dumpFileContent.Contains("TypeRefProps [01000001]: Scope: 23000001 'System.Diagnostics.DebuggableAttribute'"));
-            Assert.IsTrue(dumpFileContent.Contains(": Scope: 23000002 'TestNFClassLibrary.ClassOnAnotherAssembly'"));
+            Assert.IsTrue(dumpFileContent.Contains("TypeRefProps [01000001]: Scope: 23000001 'System.Diagnostics.DebuggableAttribute'"), "Wrong entry for System.Diagnostics.DebuggableAttribute in type ref");
+            Assert.IsTrue(dumpFileContent.Contains(": Scope: 23000002 'TestNFClassLibrary.ClassOnAnotherAssembly'"), "Wrong entry for TestNFClassLibrary.ClassOnAnotherAssembly in type ref");
 
-            Assert.IsTrue(dumpFileContent.Contains(": Flags: 00001001 Extends: 0100000f Enclosed: 02000000 'TestNFApp.DummyCustomAttribute1'"));
+            Assert.IsTrue(dumpFileContent.Contains(": Flags: 00001001 Extends: 0100000f Enclosed: 02000000 'TestNFApp.DummyCustomAttribute1'"), "Wrong entry for TestNFApp.DummyCustomAttribute1 in type ref");
 
-            Assert.IsTrue(dumpFileContent.Contains(": Flags: 00001001 Extends: 0100000f Enclosed: 02000000 'TestNFApp.DummyCustomAttribute2'"));
+            Assert.IsTrue(dumpFileContent.Contains(": Flags: 00001001 Extends: 0100000f Enclosed: 02000000 'TestNFApp.DummyCustomAttribute2'"), "Wrong entry for TestNFApp.DummyCustomAttribute2 in type ref");
 
-            Assert.IsTrue(dumpFileContent.Contains(": Flags: 00001061 Extends: 01000000 Enclosed: 02000000 'TestNFApp.IOneClassOverAll'"));
-            Assert.IsTrue(dumpFileContent.Contains(": Flags: 000007c6 Impl: 00000000 RVA: 00000000 'get_DummyProperty' [I4(   )]"));
-            Assert.IsTrue(dumpFileContent.Contains(": Flags: 000007c6 Impl: 00000000 RVA: 00000000 'set_DummyProperty' [VOID( I4 )]"));
-            Assert.IsTrue(dumpFileContent.Contains(": Flags: 000003c6 Impl: 00000000 RVA: 00000000 'DummyMethod' [VOID(   )]"));
+            Assert.IsTrue(dumpFileContent.Contains(": Flags: 00001061 Extends: 01000000 Enclosed: 02000000 'TestNFApp.IOneClassOverAll'"), "Wrong entry for TestNFApp.IOneClassOverAll in type ref");
+            Assert.IsTrue(dumpFileContent.Contains(": Flags: 000007c6 Impl: 00000000 RVA: 00000000 'get_DummyProperty' [I4( )]"), "Wrong entry for get_DummyProperty in type ref");
+            Assert.IsTrue(dumpFileContent.Contains(": Flags: 000007c6 Impl: 00000000 RVA: 00000000 'set_DummyProperty' [VOID(I4)]"), "Wrong entry for set_DummyProperty in type ref");
+            Assert.IsTrue(dumpFileContent.Contains(": Flags: 000003c6 Impl: 00000000 RVA: 00000000 'DummyMethod' [VOID( )]"), "Wrong entry for DummyMethod in type ref");
 
-            Assert.IsTrue(dumpFileContent.Contains("Enclosed: 02000000 'TestNFApp.OneClassOverAll'"));
-            Assert.IsTrue(dumpFileContent.Contains(": Attr: 00000001 Flags: 00000001 'dummyField' [STRING]"));
-            Assert.IsTrue(dumpFileContent.Contains(": Attr: 00000001 Flags: 00000001 '<DummyProperty>k__BackingField' [I4]"));
-            Assert.IsTrue(dumpFileContent.Contains(": Flags: 00000086 Impl: 00000000 RVA: 00000000 'DummyExternMethod' [VOID(   )]"));
+            Assert.IsTrue(dumpFileContent.Contains("Enclosed: 02000000 'TestNFApp.OneClassOverAll'"), "Wrong entry for TestNFApp.OneClassOverAll in type ref");
+            Assert.IsTrue(dumpFileContent.Contains(": Attr: 00000001 Flags: 00000001 'dummyField' [STRING]"), "Wrong entry for dummyField in type ref");
+            Assert.IsTrue(dumpFileContent.Contains(": Attr: 00000001 Flags: 00000001 '<DummyProperty>k__BackingField' [I4]"), "Wrong entry for <DummyProperty>k__BackingField in type ref");
+            Assert.IsTrue(dumpFileContent.Contains(": Flags: 00000086 Impl: 00000000 RVA: 00000000 'DummyExternMethod' [VOID( )]"), "Wrong entry for DummyExternMethod in type ref");
 
-            Assert.IsTrue(dumpFileContent.Contains("'TestNFApp.Program'"));
+            Assert.IsTrue(dumpFileContent.Contains("'TestNFApp.Program'"), "Wrong entry for TestNFApp.Program in type ref");
 
-            Assert.IsTrue(dumpFileContent.Contains("'SubClass'"));
+            Assert.IsTrue(dumpFileContent.Contains("'SubClass'"), "Wrong entry for SubClass in type ref");
+
+            Assert.IsTrue(Regex.IsMatch(dumpFileContent, @"Locals \( \[0\] I4, \r\n\s+\[1\] CLASS System\.Exception \[\d{8}\], \r\n\s+\[2\] CLASS System\.ApplicationException \[\d{8}\] \)"), "Wrong listing of locals in UglyAdd method");
+            Assert.IsTrue(dumpFileContent.Contains("callvirt    System.Void TestNFApp.TestingDelegates/SimpleDelegate::Invoke(System.String)"), "Wrong reference to callvirt in DelegateTests body");
+            Assert.IsTrue(dumpFileContent.Contains("ldstr       \"          DataRowAttribute.Arg[{0}] has: {1}\""), "Wrong reference to ldstr in ReflectionTests body");
 
             // UserStrings
-            Assert.IsTrue(dumpFileContent.Contains(": 'TestNFClassLibrary'"));
-            Assert.IsTrue(dumpFileContent.Contains(": 'get_DummyProperty'"));
-            Assert.IsTrue(dumpFileContent.Contains(": 'blabla'"));
+            Assert.IsTrue(dumpFileContent.Contains(": 'TestNFClassLibrary'"), "Wrong entry for TestNFClassLibrary in user string");
+            Assert.IsTrue(dumpFileContent.Contains(": 'get_DummyProperty'"), "Wrong entry for get_DummyProperty in user string");
+            Assert.IsTrue(dumpFileContent.Contains(": 'blabla'"), "Wrong entry for blabla in user string");
         }
     }
 }
