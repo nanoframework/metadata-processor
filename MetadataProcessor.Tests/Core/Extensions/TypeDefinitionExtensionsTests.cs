89c14092280f1a1a7b350cbcb53730e59c4c9763
José Simões <jose.simoes@eclo.solutions>
2025-02-25 00:16:27 +0000

    Add processing of exclude type attribute (#179)
    ***NO_CI***

--- MetadataProcessor.Tests/Core/Extensions/TypeDefinitionExtensionsTests.cs
+++ MetadataProcessor.Tests/Core/Extensions/TypeDefinitionExtensionsTests.cs
@@ -7,6 +7,9 @@ using System;
 using System.Linq;
 using nanoFramework.Tools.MetadataProcessor.Core.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mono.Cecil;
+using System.IO;
+using System.Collections.Generic;
 
 namespace nanoFramework.Tools.MetadataProcessor.Tests.Core.Extensions
 {
@@ -58,7 +61,6 @@ namespace nanoFramework.Tools.MetadataProcessor.Tests.Core.Extensions
         public void IsClassToExcludeTest()
         {
             var nanoTablesContext = TestObjectHelper.GetTestNFAppNanoTablesContext();
-            Assert.IsFalse(nanoTablesContext.ClassNamesToExclude.Any());
 
             var oneClassOverAllTypeDefinition = TestObjectHelper.GetTestNFAppOneClassOverAllTypeDefinition(nanoTablesContext.AssemblyDefinition);
             var oneClassOverAllSubClassTypeDefinition = TestObjectHelper.GetTestNFAppOneClassOverAllSubClassTypeDefinition(nanoTablesContext.AssemblyDefinition);
@@ -91,6 +93,82 @@ namespace nanoFramework.Tools.MetadataProcessor.Tests.Core.Extensions
             r = oneClassOverAllSubClassTypeDefinition.IsToExclude();
 
             Assert.IsTrue(r);
+        }
+
+        [TestMethod]
+        public void TypesInLibraryToExcludeTests()
+        {
+            // Arrange
+            string libLocation = Path.Combine(
+                TestObjectHelper.TestExecutionLocation,
+                "TestNFClassLibrary");
+
+            string libAssemblyLocation = Path.Combine(
+                libLocation,
+               "TestNFClassLibrary.dll");
+
+            AssemblyDefinition assemblyDefinition = AssemblyDefinition.ReadAssembly(libAssemblyLocation);
+
+            nanoTablesContext nanoTablesContext = new nanoTablesContext(
+                assemblyDefinition,
+                null,
+                // adding this here, equivalent to the NFMDP_PE_ExcludeClassByName project property
+                new List<string>() { "TestNFClassLibrary.IAmATypeToExclude" },
+                null,
+                false,
+                false,
+                false);
+
+            ModuleDefinition testNFLibraryModule = nanoTablesContext.AssemblyDefinition.Modules[0];
+
+            // Assert
+
+            // test if <TestNFClassLibrary.ClassOnAnotherAssembly> type is present
+            Assert.IsNotNull(testNFLibraryModule.Types.FirstOrDefault(i => i.FullName == "TestNFClassLibrary.ClassOnAnotherAssembly"), "TestNFClassLibrary.ClassOnAnotherAssembly type not found");
+
+            // test if <TestNFClassLibrary.IAmATypeToExclude> type is present
+            Assert.IsNotNull(testNFLibraryModule.Types.FirstOrDefault(i => i.FullName == "TestNFClassLibrary.IAmATypeToExclude"), "TestNFClassLibrary.IAmATypeToExclude type not found");
+
+            // test if <TestNFClassLibrary.IAmATypeToExclude> type is to be excluded
+            // this is being excluded with the NFMDP_PE_ExcludeClassByName project property
+            Assert.IsTrue(testNFLibraryModule.Types.First(i => i.FullName == "TestNFClassLibrary.IAmATypeToExclude").IsToExclude(), "TestNFClassLibrary.IAmATypeToExclude type is not listed to be excluded, when it should");
+
+            // test if <TestNFClassLibrary.IAmAlsoATypeToExclude> type is present
+            Assert.IsNotNull(testNFLibraryModule.Types.FirstOrDefault(i => i.FullName == "TestNFClassLibrary.IAmAlsoATypeToExclude"), "TestNFClassLibrary.IAmAlsoATypeToExclude type not found");
+
+            // test if <TestNFClassLibrary.IAmAlsoATypeToExclude> type is to be excluded
+            // this is being excluded with the ExcludeType attribute
+            Assert.IsTrue(testNFLibraryModule.Types.First(i => i.FullName == "TestNFClassLibrary.IAmAlsoATypeToExclude").IsToExclude(), "TestNFClassLibrary.IAmAlsoATypeToExclude type is not listed to be excluded, when it should");
+
+            // test if <TestNFClassLibrary.IAmAnEnumToExclude> type is present
+            Assert.IsNotNull(testNFLibraryModule.Types.FirstOrDefault(i => i.FullName == "TestNFClassLibrary.IAmAnEnumToExclude"), "TestNFClassLibrary.IAmAnEnumToExclude type not found");
+
+            // test if <TestNFClassLibrary.IAmAnEnumToExclude> type is to be excluded
+            // this is being excluded with the ExcludeType attribute
+            Assert.IsTrue(testNFLibraryModule.Types.First(i => i.FullName == "TestNFClassLibrary.IAmAnEnumToExclude").IsToExclude(), "TestNFClassLibrary.IAmAnEnumToExclude type is not listed to be excluded, when it should");
+        }
+
+        [TestMethod]
+        public void DefaultTypesToExcludeTests()
+        {
+            // Arrange
+            AssemblyDefinition mscorlibAssemblyDefinition = AssemblyDefinition.ReadAssembly(TestObjectHelper.MscorlibFullPath);
+
+            nanoTablesContext context = new nanoTablesContext(
+                mscorlibAssemblyDefinition,
+                null,
+                new List<string>(),
+                null,
+                false,
+                false,
+                true);
+
+            ModuleDefinition mscorlibModule = context.AssemblyDefinition.Modules[0];
+
+            // Assert
+            Assert.IsNotNull(context);
+
+            Assert.IsTrue(mscorlibModule.Types.First(i => i.FullName == "ThisAssembly").IsToExclude(), "ThisAssembly type is not listed to be excluded, when it should");
         }
     }
 }
---
+++ MetadataProcessor.Tests/TestNFClassLibrary/TestNFClassLibrary/IAmATypeToExclude.cs
@@ -0,0 +1,12 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace TestNFClassLibrary
+{
+    // this type does nothing, its here to test the backwards compatibility with NFMDP_PE_ExcludeClassByName project property to exclude types
+    public class IAmATypeToExclude
+    {
+    }
+}
