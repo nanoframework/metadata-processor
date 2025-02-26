16a14a592a518229f5fad790ff581992c82dd03a
José Simões <jose.simoes@eclo.solutions>
2025-02-26 16:03:40 +0000

    Fix processing of auto backing fields (#180)

---
+++ MetadataProcessor.Tests/TestNFClassLibrary/TestNFClassLibrary/ClassWithNativeImplementation.cs
@@ -0,0 +1,35 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System.Runtime.CompilerServices;
+
+namespace TestNFClassLibrary
+{
+    public class ClassWithNativeImplementation
+    {
+        private static int _staticField;
+        private int _field;
+
+        public static int StaticProperty1 { get; set; }
+
+        public int Property1 { get; set; }
+
+        public void ManagedMethod1()
+        {
+            NativeMethod1();
+        }
+
+        public void ManagedMethod2()
+        {
+            NativeMethod2();
+        }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void NativeMethod1();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void NativeMethod2();
+    }
+}
